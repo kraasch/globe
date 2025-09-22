60ee912960b0d13c4e7f5d9e2d149a385dad904c
Alex Kraasch <alex@kraasch.eu>
2025-09-22 22:23:32 +0200

    Add new test stub.

--- pkg/geocalc/main.go
+++ pkg/geocalc/main.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"time"
 
-	sunrise "github.com/nathan-osman/go-sunrise"
+	mp "github.com/janczer/goMoonPhase"
+	sr "github.com/nathan-osman/go-sunrise"
 )
 
+func MoonPhase(date time.Time) string {
+	phase := mp.New(date)
+	return fmt.Sprintf("%#v", phase)
+}
+
 func SunRiseAndSet(lat, lon float64, date time.Time) string {
-	rise, set := sunrise.SunriseSunset(lat, lon, date.Year(), date.Month(), date.Day())
+	rise, set := sr.SunriseSunset(lat, lon, date.Year(), date.Month(), date.Day())
 	// TODO: convert lat and lon to timezone string.
 	localization := time.FixedZone("GMT-5", -5*60*60) // TODO: extract this into a universal function which can return any time zone depending on lon and lat.
 	localRise := rise.In(localization)
--- pkg/geocalc/main_test.go
+++ pkg/geocalc/main_test.go
@@ -28,7 +28,49 @@ type TestSuite struct {
 	tests           []TestList
 }
 
-var suites = []TestSuite{
+var suites = []TestSuite{ // All tests.
+
+	/*
+	 * Test for the function MoonPhase().
+	 */
+	{
+		testingFunction: func(in TestList) string {
+			time, err0 := time.Parse(dateLayout, in.inputArr[0])
+			if err0 != nil {
+				return "error in type converstion within the test: date."
+			}
+			out := MoonPhase(time)
+			return out
+		},
+		tests: []TestList{
+			{
+				testName: "sun_sunrise+sunset_calculate_00",
+				isMulti:  false,
+				inputArr: []string{
+					"2000-01-01", // Some date.
+				},
+				expectedValue: //
+				"The moon is currently AGE days old, and is therefore PHASE " + NL +
+					"It is DIST km from the centre of the Earth." + NL +
+					"It is ILLUM % illuminated." + NL +
+					"The next new moon is at NEXT.",
+				/* NOTE: other values are:
+				 *  - PHASE:     0.8255741837703208
+				 *  - illum:     0.27139898737765766
+				 *  - AGE:       24.379691645748075
+				 *  - DIST:      398596.29455439356
+				 *  - angdia:    0.49964879458462286
+				 *  - sundist:   1.4710022336390877e+08
+				 *  - sunangdia: 0.5421823793611453
+				 *  - pdata:     2.4515445e+06
+				 *  - quarters:  [8]float64{9.446059836222186e+08, 9.453054569995925e+08, 9.458839879086032e+08, 9.46476358262685e+08, 9.471824973697052e+08, 9.478568800435827e+08, 9.484297056796163e+08, 9.490463221789911e+08}
+				 *  - timespace: 9.466848e+08
+				 *  - longitude:217.09009941697522
+				 */
+			},
+		},
+	},
+
 	/*
 	 * Test for the function SunRiseAndSet().
 	 */
