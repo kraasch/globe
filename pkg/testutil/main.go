6f8d09268d76f99e7f7e8a6b4dd740458552b1d7
Alex Kraasch <alex@kraasch.eu>
2025-09-24 22:19:18 +0200

    Add more restrictive military time regex.

--- pkg/testutil/main.go
+++ pkg/testutil/main.go
@@ -5,9 +5,9 @@ import (
 	"regexp"
 )
 
-// AnonymizeTimeStrings takes a string and replaces times like 07:32 or 24:20 into xx:yy for time-independent string tests.
+// AnonymizeTimeStrings takes a string and replaces times like 07:32 or 23:59 into xx:yy for time-independent string tests.
 func AnonymizeTimeStrings(timestr string) string {
-	pattern := `[0-9]{2}:[0-9]{2}`
+	pattern := `(?:[01][0-9]|2[0-3]):[0-5][0-9]` // more restrictive pattern.
 	re := regexp.MustCompile(pattern)
 	result := re.ReplaceAllString(timestr, "xx:yy")
 	return result
